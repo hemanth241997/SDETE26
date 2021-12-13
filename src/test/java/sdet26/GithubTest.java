14c99ae586464930bcbdcce1db09ce540ecc9759
hamanth <hamanth@hamanth-PC>
2021-12-13 13:00:18 +0530

    PUSH

--- src/test/java/sdet26/GithubTest.java
+++ src/test/java/sdet26/GithubTest.java
@@ -2,20 +2,14 @@ package sdet26;
 
 import org.testng.annotations.Test;
 
-public class github {
+public class GithubTest {
 	@Test
 	public void gitHub() {
-		System.out.println("push and commit");
-		System.out.println("pull");
 		System.out.println("pruthvi");
 		System.out.println("puneeth rajkumar");
 		System.out.println("richi");
-		System.out.println("cherli");
 		System.out.println("harshitha");
-		System.out.println("pru");
-		System.out.println("xyz");
-		
-		
+		System.out.println("pru");		
 	}
 
 }
