436d4a936c81c4112f2ad0c0a266bf53a607e397
hamanth <hamanth@hamanth-PC>
2021-12-13 13:24:08 +0530

    appu

---
+++ src/test/java/sdet26/practice2.java
@@ -0,0 +1,13 @@
+package sdet26;
+
+import org.testng.annotations.Test;
+
+public class practice2 {
+	
+@Test
+public void practice2() {
+	System.out.println("Appu");
+	System.out.println("james");
+}
+
+}
